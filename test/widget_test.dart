92e1c0113930d21010a3e1ecfa95afd3c561b051
Vikram Pratap Singh <vikram@geekyants.com>
2019-03-11 18:16:39 +0530

    edit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,9 @@
+import 'package:animation/Screens/home.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
   testWidgets("Home screen has a my painter logo",(WidgetTester tester)async{
+    tester.pumpWidget(Home());
     final titleFinder = find.text('My Painter');
     expect(titleFinder, findsOneWidget);
   });
