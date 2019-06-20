302aae39d4e27296c88c0fa2181dbe06faa0a45b
Vikram Pratap Singh <vikram@geekyants.com>
2019-06-20 12:23:34 +0530

    testing codemagic 2

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -122,7 +122,7 @@ class Home extends StatelessWidget{
                       children: <Widget>[
                         SizedBox(height: 20,),
                         CustomText.text18(
-                          text:"Interior",
+                          text:"Interior Alone",
                         ),
                         SizedBox(height: 40,),
                         customContainer(
